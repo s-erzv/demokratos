2ed2f28e08f0230763d3339852bb226b986b122d
amdihsann <ahmadihsan506@gmail.com>
2025-09-16 20:12:59 +0700

    API KEY

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './firebase.js'
 import './global.css';
 
 import Dashboard from './pages/Dashboard';
